27e60174cb1841c4e5d2a5f7c1818f9f43708563
risbakk <eric.risbakk@gmail.com>
2019-11-10 15:12:04 +0100

    Tried using external code for MCTS, but since I don't want to spend the time figuring out why it doesn't work I'm just going to code my own now.

--- mcts-master/src/main/java/io/github/nejc92/mcts/MctsDomainAgent.java
+++
@@ -1,7 +0,0 @@
-package io.github.nejc92.mcts;
-
-public interface MctsDomainAgent<StateT extends MctsDomainState> {
-
-    StateT getTerminalStateByPerformingSimulationFromState(StateT state);
-    double getRewardFromTerminalState(StateT terminalState);
-}
