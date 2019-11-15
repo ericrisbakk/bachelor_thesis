971fadb17e81e9bcd26a5677868b5f5c26dbc8e7
risbakk <eric.risbakk@gmail.com>
2019-11-15 13:51:09 +0100

    New name for State.

--- terminus_est/src/mcts/NodeMCTS.java
+++ terminus_est/src/mcts/NodeMCTS.java
@@ -6,12 +6,14 @@ import java.util.Collection;
  * Represents the collection of values which defines an instance of some object, as well
  * as an MCTS node.
  */
-public interface State {
+public interface NodeMCTS {
     /**
      * Apply action to this state.
      * @param a Given action. It should always be a legal w.r.t. State params.
      */
     void Apply(Action a);
+
+
 
     /**
      * Create ActionStatePair for all allowed actions and store to this object.
@@ -22,17 +24,17 @@ public interface State {
      * Fetch results from expansion.
      * @return Some collection of
      */
-    Collection<ActionStatePair> GetActionsAndChildren();
+    Collection<NodeMCTS> GetChildren();
 
     /**
      * Get parent in search tree.
      * @return parent state-node.
      */
-    State GetParent();
+    NodeMCTS GetParent();
 
     /**
      * Creates a deep copy of this object.
      * @return Deep copy.
      */
-    State DeepCopy();
+    NodeMCTS DeepCopy();
 }
