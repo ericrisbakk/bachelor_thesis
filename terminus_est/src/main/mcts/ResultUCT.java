671b73f13f86b80b12e95468cd43b5793f680359
risbakk <eric.risbakk@gmail.com>
2019-11-17 18:08:47 +0100

    implement interface.

--- terminus_est/src/main/mcts/ResultUCT.java
+++ terminus_est/src/main/mcts/ResultUCT.java
@@ -1,9 +1,19 @@
 package main.mcts;
 
+import main.mcts.base.IResult;
+
+import javax.xml.transform.Result;
+
 /**
  * Logs the necessary information for doing single-player UCT-MCTS.
  */
-public class ResultUCT {
+public class ResultUCT implements IResult {
     int wins = 0;
     int simulations = 0;
+
+    @Override
+    public void Update(IResult newResult) {
+        wins += ((ResultUCT) newResult).wins;
+        simulations += ((ResultUCT) newResult).simulations;
+    }
 }
