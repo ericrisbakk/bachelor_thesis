195b442fd7944bc8164f8e772f853ba9b1e04443
risbakk <eric.risbakk@gmail.com>
2019-11-17 18:32:10 +0100

    Also, TTT state workings.

--- terminus_est/src/main/mcts/base/State.java
+++ terminus_est/src/main/mcts/base/State.java
@@ -1,4 +1,17 @@
 package main.mcts.base;
 
+/**
+ * Symbolizes a game state.
+ */
 public interface State {
+    /**
+     * Apply given action to the state (does not create a new copy of the state).
+     * @param a A legal action.
+     */
+    void Apply(Action a);
+
+    /**
+     * @return Get all possible legal actions for the current state.
+     */
+    Action[] GetLegalActions();
 }
--- terminus_est/src/test/tictactoe/TicTacToeState.java
+++ terminus_est/src/test/tictactoe/TicTacToeState.java
@@ -1,4 +1,47 @@
 package test.tictactoe;
 
-public class TicTacToeState {
+import main.mcts.base.Action;
+import main.mcts.base.State;
+
+public class TicTacToeState implements State {
+    public int[][] board;
+    public int player;
+    public int movesTotal;
+    private int player1Val = 1;
+    private int player2Val = 2;
+
+    public TicTacToeState() {
+        board = new int[3][3];
+        player = 0;
+        movesTotal = 0;
+    }
+
+    @Override
+    public void Apply(Action a) {
+        TicTacToeAction ta = (TicTacToeAction) a;
+        if (board[ta.x][ta.y] != 0) {
+            System.out.println("ERROR: Attempting to overwrite previous action.");
+            System.exit(0);
+        }
+
+        board[ta.x][ta.y] = ta.player;
+    }
+
+    @Override
+    public Action[] GetLegalActions() {
+        if (movesTotal == 9) return null;
+        int nextPlayer = player == 1 ? 2 : 1;
+        TicTacToeAction[] actions = new TicTacToeAction[9-movesTotal];
+        int added = 0;
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[i].length; ++j) {
+                if (board[i][j] == 0) {
+                    actions[added] = new TicTacToeAction(i, j, nextPlayer);
+                    ++added;
+                }
+            }
+        }
+
+        return actions;
+    }
 }
