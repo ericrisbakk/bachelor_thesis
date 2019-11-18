467189d7e6c6bf21747d6bcb5a04c1526be307e0
Eric Risbakk <e.risbakk@student.maastrichtuniversity.nl>
2019-11-18 11:43:23 +0100

    .

--- terminus_est/src/main/utility/IDeepCopy.java
+++ terminus_est/src/main/utility/IDeepCopy.java
@@ -1,4 +1,11 @@
 package main.utility;
 
+/**
+ * Object can create a deep copy of itself.
+ */
 public interface IDeepCopy {
+    /**
+     * @return Copy of object that is separate from the original.
+     */
+    IDeepCopy DeepCopy();
 }
