ab8e155e61de6d86253dbb16518e6b5f5746a1a1
risbakk <eric.risbakk@gmail.com>
2019-11-18 17:12:52 +0100

    move method and fix oversight w.r.t. player turn in apply.

--- terminus_est/src/test/tictactoe/TicTacToeState.java
+++ terminus_est/src/test/tictactoe/TicTacToeState.java
@@ -25,19 +25,27 @@ public class TicTacToeState implements State {
             System.exit(0);
         }
 
+        // Update board.
         board[ta.x][ta.y] = ta.player;
+        ++ movesTotal;
+
+        // Set new player.
+        if (ta.player == 1)
+            player = 2;
+        else
+            player = 1;
     }
 
     @Override
     public Action[] GetLegalActions() {
         if (movesTotal == 9) return null;
-        int nextPlayer = player == 1 ? 2 : 1;
+
         TicTacToeAction[] actions = new TicTacToeAction[9-movesTotal];
         int added = 0;
         for (int i = 0; i < board.length; ++i) {
             for (int j = 0; j < board[i].length; ++j) {
                 if (board[i][j] == 0) {
-                    actions[added] = new TicTacToeAction(i, j, nextPlayer);
+                    actions[added] = new TicTacToeAction(i, j, player);
                     ++added;
                 }
             }
@@ -71,21 +79,6 @@ public class TicTacToeState implements State {
         newState.movesTotal = movesTotal;
 
         return newState;
-    }
-
-    @Override
-    public String toString() {
-        String s = "";
-        for (int i = 0; i < board.length; ++i) {
-            for (int j = 0; j < board[i].length; ++j) {
-                s += " " + board[i][j] + " ";
-            }
-
-            s += "\n";
-        }
-        s +="\nPlayer: " + player;
-
-        return s;
     }
 
     /**
@@ -127,5 +120,20 @@ public class TicTacToeState implements State {
             return board[0][2];
 
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        String s = "Board:\n";
+        for (int i = 0; i < board.length; ++i) {
+            for (int j = 0; j < board[i].length; ++j) {
+                s += " " + board[i][j] + " ";
+            }
+
+            s += "\n";
+        }
+        s +="\nPlayer: " + player;
+
+        return s;
     }
 }
