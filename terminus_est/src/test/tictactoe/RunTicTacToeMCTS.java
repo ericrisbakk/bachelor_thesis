654c8b12243f47a358b2c06e47f2008251415493
risbakk <eric.risbakk@gmail.com>
2019-11-18 15:15:33 +0100

    Finished the game. Let's see how it goes.

--- terminus_est/src/test/tictactoe/RunTicTacToeMCTS.java
+++ terminus_est/src/test/tictactoe/RunTicTacToeMCTS.java
@@ -2,11 +2,47 @@ package test.tictactoe;
 
 import main.mcts.SelectUCT;
 import main.mcts.SimulateRandom;
+import main.mcts.base.Action;
 import main.mcts.base.MCTS;
+import java.util.Scanner;  // Import the Scanner class
 
 public class RunTicTacToeMCTS {
+
+
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
         TicTacToePlayer1Wins h = new TicTacToePlayer1Wins();
-        MCTS mcts = new MCTS(new TicTacToeState(), 100, new SelectUCT(), new SimulateRandom(h));
+        TicTacToeState game = new TicTacToeState();
+        MCTS mcts = new MCTS(100, new SelectUCT(), new SimulateRandom(h));
+
+        s.next();
+
+        while (!game.EndState()) {
+            System.out.println(game.toString());
+            TicTacToeAction a;
+            if (game.player == 1) {
+                mcts.BuildTree(game);
+                a = (TicTacToeAction) mcts.GetBestAction();
+            }
+            else {
+                System.out.println("\nTurn: " + game.movesTotal);
+                Action[] legal = game.GetLegalActions();
+                System.out.println("Choose: \n");
+                for (int i = 0; i < legal.length; ++i) {
+                    System.out.println(i +" - " + legal[i].toString() + "\n");
+                }
+
+                int inp;
+                do {
+                    inp = s.nextInt();
+                } while (inp >= 0 && inp < legal.length);
+                a = (TicTacToeAction) legal[inp];
+            }
+
+            game.Apply(a);
+        }
+
+        System.out.println("\nWINNER: " + game.Winner());
     }
 }
