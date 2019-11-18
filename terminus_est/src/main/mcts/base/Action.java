875de16dc134fca05a89357350ee20817319f56a
Eric Risbakk <e.risbakk@student.maastrichtuniversity.nl>
2019-11-18 11:43:35 +0100

    .

--- terminus_est/src/main/mcts/base/Action.java
+++ terminus_est/src/main/mcts/base/Action.java
@@ -1,9 +1,11 @@
 package main.mcts.base;
 
+import main.utility.IDeepCopy;
+
 /**
  * Represents any discrete event which may change the state.
  * Ensure that a 'no-action' (e.g. passing your turn) is also encoded as a possible action in this,
  * as a Null object for an action is used to keep track of the root node.
  */
-public interface Action {
+public interface Action extends IDeepCopy {
 }
--- terminus_est/src/main/mcts/base/State.java
+++ terminus_est/src/main/mcts/base/State.java
@@ -1,9 +1,11 @@
 package main.mcts.base;
 
+import main.utility.IDeepCopy;
+
 /**
  * Symbolizes a game state.
  */
-public interface State {
+public interface State extends IDeepCopy {
     /**
      * Apply given action to the state (does not create a new copy of the state).
      * @param a A legal action.
