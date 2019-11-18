e390093ed33efb1099ddbec736fb2a9c5bf15159
risbakk <eric.risbakk@gmail.com>
2019-11-18 14:13:49 +0100

    Class to test TTT

--- terminus_est/src/test/tictactoe/RunTicTacToeMCTS.java
+++ terminus_est/src/test/tictactoe/RunTicTacToeMCTS.java
@@ -1,4 +1,10 @@
 package test.tictactoe;
 
+import main.mcts.SelectUCT;
+import main.mcts.base.MCTS;
+
 public class RunTicTacToeMCTS {
+    public static void main(String[] args) {
+        MCTS mcts = new MCTS(new TicTacToeState(), 100, new SelectUCT(), null);
+    }
 }
