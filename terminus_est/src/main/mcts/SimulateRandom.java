c038836f5ba5ea8bdbc7eb028bb822301ad5a831
risbakk <eric.risbakk@gmail.com>
2019-11-18 14:11:15 +0100

    Simulate random for anything.

--- terminus_est/src/main/mcts/SimulateRandom.java
+++ terminus_est/src/main/mcts/SimulateRandom.java
@@ -1,4 +1,15 @@
 package main.mcts;
 
-public class SimulateRandom {
+import main.mcts.base.INodeMCTS;
+import main.mcts.base.IResult;
+import main.mcts.base.ISimulationPolicy;
+import main.mcts.base.State;
+
+public class SimulateRandom implements ISimulationPolicy {
+    @Override
+    public IResult Simulate(INodeMCTS node) {
+        State s = ((NodeMCTS) node).ConstructNodeState();
+
+        return null;
+    }
 }
