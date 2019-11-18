e72dc2374557faedc56160b57c9b0feb362a0a84
risbakk <eric.risbakk@gmail.com>
2019-11-18 15:40:06 +0100

    Finish random sim.

--- terminus_est/src/main/mcts/SimulateRandom.java
+++ terminus_est/src/main/mcts/SimulateRandom.java
@@ -2,17 +2,38 @@ package main.mcts;
 
 import main.mcts.base.*;
 
-public class SimulateRandom implements ISimulationPolicy {
-    IHeuristic heuristicP1Wins;
+import java.util.Random;
 
-    public SimulateRandom(IHeuristic heuristicP1Wins) {
+public class SimulateRandom implements ISimulationPolicy {
+    Random r = new Random();
+    IHeuristic heuristicP1Wins;
+    int simulations;
+
+    public SimulateRandom(int simulations, IHeuristic heuristicP1Wins) {
         this.heuristicP1Wins = heuristicP1Wins;
+        this.simulations = simulations;
     }
 
     @Override
     public IResult Simulate(INodeMCTS node) {
         State s = ((NodeMCTS) node).ConstructNodeState();
+        ResultUCT r = new ResultUCT();
 
-        return null;
+        for (int i = 0; i < simulations; ++i) {
+            State sim = (State) s.DeepCopy();
+            while (!sim.EndState()) {
+                sim.Apply( GetRandomAction(sim.GetLegalActions()) );
+            }
+
+            double val = heuristicP1Wins.Calculate(sim);
+            r.wins += val;
+            r.simulations += 1;
+        }
+
+        return r;
+    }
+
+    public Action GetRandomAction(Action[] actions) {
+        return actions[r.nextInt(actions.length)];
     }
 }
