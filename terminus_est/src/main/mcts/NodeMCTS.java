63eb91d2cc4aec9a2144ae84fc66059097845415
Eric Risbakk <e.risbakk@student.maastrichtuniversity.nl>
2019-11-18 11:04:22 +0100

    .

--- terminus_est/src/main/mcts/NodeMCTS.java
+++ terminus_est/src/main/mcts/NodeMCTS.java
@@ -1,4 +1,62 @@
 package main.mcts;
 
-public class NodeMCTS {
+import main.mcts.base.Action;
+import main.mcts.base.INodeMCTS;
+import main.mcts.base.IResult;
+
+public class NodeMCTS implements INodeMCTS {
+    @Override
+    public Action GetLastAction() {
+        return null;
+    }
+
+    @Override
+    public IResult GetResult() {
+        return null;
+    }
+
+    @Override
+    public void Expand() {
+
+    }
+
+    @Override
+    public INodeMCTS[] GetChildren() {
+        return new INodeMCTS[0];
+    }
+
+    @Override
+    public INodeMCTS GetParent() {
+        return null;
+    }
+
+    @Override
+    public INodeMCTS DeepCopy() {
+        return null;
+    }
+
+    @Override
+    public boolean IsExpanded() {
+        return false;
+    }
+
+    @Override
+    public boolean IsLeaf() {
+        return false;
+    }
+
+    @Override
+    public boolean IsTerminal() {
+        return false;
+    }
+
+    @Override
+    public int ExpandedChildCount() {
+        return 0;
+    }
+
+    @Override
+    public int ChildCount() {
+        return 0;
+    }
 }
