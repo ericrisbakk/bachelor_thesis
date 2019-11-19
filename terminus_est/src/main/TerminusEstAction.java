738c46a544ab68e9d3c08b792d6ae4934c68adf0
risbakk <eric.risbakk@gmail.com>
2019-11-19 20:54:31 +0100

    Keep track of the taxon we want to delete.

--- terminus_est/src/main/TerminusEstAction.java
+++ terminus_est/src/main/TerminusEstAction.java
@@ -1,12 +1,21 @@
 package main;
 
+import main.TerminusEst.Tree;
 import main.mcts.base.Action;
 import main.utility.IDeepCopy;
 
+
 public class TerminusEstAction implements Action {
+    Tree taxon;
+
+    public TerminusEstAction(Tree taxon) {
+        this.taxon = taxon;
+    }
+
+    private TerminusEstAction() { }
 
     @Override
     public IDeepCopy DeepCopy() {
-        return null;
+        return new TerminusEstAction(taxon);
     }
 }
