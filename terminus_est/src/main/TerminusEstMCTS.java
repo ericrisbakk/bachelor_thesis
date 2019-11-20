f022d4529c8d8ce365e8bab56cfd8e2d7d2236fd
risbakk <eric.risbakk@gmail.com>
2019-11-20 14:17:01 +0100

    The main part of the algorithm.

--- terminus_est/src/main/TerminusEstMCTS.java
+++ terminus_est/src/main/TerminusEstMCTS.java
@@ -1,4 +1,31 @@
 package main;
 
+import main.TerminusEst.TerminusEstInputHandler;
+import main.TerminusEst.TerminusEstState;
+import main.TerminusEst.TerminusEstV4;
+import main.TerminusEst.Tree;
+import main.mcts.HeuristicAvgDepth;
+import main.mcts.ResultUCTGenerator;
+import main.mcts.SelectUCT;
+import main.mcts.SimulateRandom;
+import main.mcts.base.MCTS;
+
 public class TerminusEstMCTS {
+
+    public static void main(String[] args) {
+        SelectUCT select = new SelectUCT;
+        HeuristicAvgDepth heuristic = new HeuristicAvgDepth();
+        SimulateRandom sim = new SimulateRandom(30, heuristic);
+        ResultUCTGenerator gen = new ResultUCTGenerator();
+        MCTS mcts = new MCTS(1000, select, sim, gen);
+
+        TerminusEstInputHandler inp = new TerminusEstInputHandler();
+        inp.InterpretFile(args[0]);
+        TerminusEstV4 te4 = new TerminusEstV4();
+
+        Tree T1 = TerminusEstV4.t1.copy(null, null);
+        Tree T2 = TerminusEstV4.t2.copy(null, null);
+        TerminusEstState state = new TerminusEstState(T1, T2, 0);
+
+    }
 }
