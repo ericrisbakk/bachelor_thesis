20327aeade92a0312f3e2e1c694ca16174af9c67
risbakk <eric.risbakk@gmail.com>
2019-11-20 14:06:26 +0100

    .

--- terminus_est/src/main/mcts/HeuristicAvgDepth.java
+++ terminus_est/src/main/mcts/HeuristicAvgDepth.java
@@ -1,4 +1,28 @@
 package main.mcts;
 
-public class HeuristicAvgDepth {
+import main.TerminusEst.TerminusEstState;
+import main.mcts.base.IHeuristic;
+import main.mcts.base.State;
+
+/**
+ * Keeps track
+ */
+public class HeuristicAvgDepth implements IHeuristic {
+
+    double sum = 0;
+    long n = 0;
+
+    @Override
+    public double Calculate(State state) {
+        TerminusEstState s = (TerminusEstState) state;
+        sum +=  s.depth;
+        n += 1;
+
+        if (s.depth < sum / n)
+            return 1;
+        else if (s.depth > sum / n)
+            return 0;
+
+        return 0.5;
+    }
 }
