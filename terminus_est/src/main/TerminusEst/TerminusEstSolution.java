198d5cf59433fd79477e57ae936dfcdbfd769dd8
risbakk <eric.risbakk@gmail.com>
2019-11-21 11:26:48 +0100

    .

--- terminus_est/src/main/TerminusEst/TerminusEstSolution.java
+++ terminus_est/src/main/TerminusEst/TerminusEstSolution.java
@@ -1,4 +1,14 @@
 package main.TerminusEst;
 
+/**
+ * A solution contains both a network, and a hybridization number.
+ */
 public class TerminusEstSolution {
+    public Network root;
+    public int hyb;
+
+    public TerminusEstSolution(Network root, int hyb) {
+        this.root = root;
+        this.hyb = hyb;
+    }
 }
--- terminus_est/src/main/utility/Tuple2.java
+++ terminus_est/src/main/utility/Tuple2.java
@@ -1,4 +1,11 @@
 package main.utility;
 
-public class Tuple2 {
+public class Tuple2<A, B> {
+    public A item1;
+    public B item2;
+
+    public Tuple2(A item1, B item2) {
+        this.item1 = item1;
+        this.item2 = item2;
+    }
 }
