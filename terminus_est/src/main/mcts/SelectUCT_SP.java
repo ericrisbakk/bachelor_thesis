b5f603eabf87064036fae41093d3febcf4ec77bd
risbakk <eric.risbakk@gmail.com>
2020-01-02 12:03:25 +0100

    UCT SP version should be working?

--- terminus_est/src/main/mcts/SelectUCT_SP.java
+++ terminus_est/src/main/mcts/SelectUCT_SP.java
@@ -3,12 +3,28 @@ package main.mcts;
 import main.mcts.base.Action;
 
 public class SelectUCT_SP extends SelectUCT {
+    public static double param_d = 0;
 
     @Override
     public double GetUCT(NodeMCTS node) {
-        ResultUCT val = (ResultUCT) node.GetResult();
-        ResultUCT pVal = (ResultUCT) node.GetParent().GetResult();
+        ResultUCT_SP val = (ResultUCT_SP) node.GetResult();
+        ResultUCT_SP pVal = (ResultUCT_SP) node.GetParent().GetResult();
 
-        return super.GetUCT(node);
+        double avg = ( (double) val.score / (double) val.simulations);
+        double uctScore = ( avg + (param_c*Math.sqrt( (Math.log(pVal.simulations))/val.simulations )));
+
+        uctScore += Math.sqrt((val.score2 - (val.simulations*Math.pow(avg, 2)) + param_d)/((double) val.simulations));
+
+        return uctScore;
+    }
+
+    @Override
+    public int GetVisits(NodeMCTS node) {
+        return (int) ((ResultUCT_SP) node.GetResult()).simulations;
+    }
+
+    @Override
+    public boolean HasBeenSimulatedFrom(NodeMCTS node) {
+        return ((ResultUCT_SP) node.GetResult()).simulations != 0;
     }
 }
