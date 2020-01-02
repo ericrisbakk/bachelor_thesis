0d461486a5bee763a648f8c34c0818ed1481746c
risbakk <eric.risbakk@gmail.com>
2020-01-02 11:43:37 +0100

    Only go by best depth found so far.

--- terminus_est/src/main/mcts/HeuristicBestDepth.java
+++ terminus_est/src/main/mcts/HeuristicBestDepth.java
@@ -1,4 +1,22 @@
 package main.mcts;
 
-public class HeuristicBestDepth {
+import main.TerminusEst.TerminusEstState;
+import main.mcts.base.IHeuristic;
+import main.mcts.base.State;
+
+public class HeuristicBestDepth implements IHeuristic {
+    public int bestDepth;
+
+
+    @Override
+    public double Calculate(State state) {
+        TerminusEstState s = (TerminusEstState) state;
+
+        if (s.depth < bestDepth)
+            return 1;
+        else if (s.depth > bestDepth)
+            return 0;
+
+        return 0.5;
+    }
 }
