8b853963ff74f8c697611446b6d853c5d43b5b16
risbakk <eric.risbakk@gmail.com>
2020-01-02 14:09:45 +0100

    .

--- terminus_est/src/main/mcts/HeuristicNegativeDepth.java
+++ terminus_est/src/main/mcts/HeuristicNegativeDepth.java
@@ -1,4 +1,12 @@
 package main.mcts;
 
-public class HeuristicNegativeDepth {
+import main.TerminusEst.TerminusEstState;
+import main.mcts.base.IHeuristic;
+import main.mcts.base.State;
+
+public class HeuristicNegativeDepth implements IHeuristic {
+    @Override
+    public double Calculate(State state) {
+        return -((TerminusEstState) state).depth;
+    }
 }
