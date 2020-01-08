14080e51ecbfb90e595f4c24491b7b07ad63930f
risbakk <eric.risbakk@gmail.com>
2020-01-08 14:24:17 +0100

    .

--- terminus_est/src/main/TerminusEst/TerminusEstParallel.java
+++ terminus_est/src/main/TerminusEst/TerminusEstParallel.java
@@ -1,4 +1,40 @@
 package main.TerminusEst;
 
-public class TerminusEstParallel {
+import java.util.concurrent.Callable;
+
+/**
+ * Used to run TerminusEst partial solution computations in parallel.
+ */
+public class TerminusEstParallel implements Callable<TerminusEstSolution> {
+
+    public TerminusEstV4 te4;
+    public Tree t1;
+    public Tree t2;
+    public int target;
+    public int depth;
+    @Override
+
+    public TerminusEstSolution call() throws Exception {
+        // System.out.println("Trying hybridization number: " + l);
+        Tree T1 = t1.copy(null,null);
+        Tree T2 = t1.copy(null,null);
+
+        Network net = te4.hybNumAtMost( T1, T2, target, t1, t2, depth );
+        if( net != null )
+        {
+            // We do not remove the fake root when computing a partial solution.
+            if( net.root.children.size() != 1 )
+            {
+                System.out.println("CATASTROPHIC ERROR, we lost the fake root...");
+                System.exit(0);
+            }
+
+            long timeEnd = System.currentTimeMillis();
+            double seconds = TerminusEstV4.getIntervalInSeconds(timeEnd, te4.startTime);
+
+            return new TerminusEstSolution(net, target, seconds);
+        }
+
+        return null;
+    }
 }
